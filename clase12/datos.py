9bf2ac3428a7af77fe367c6e6af4e97e0f8d774c
Frvnkoo <134543110+Frvnkoo@users.noreply.github.com>
2023-05-24 14:34:16 -0400

    Archivos datos.py  sin validar
    xd

---
+++ clase12/datos.py
@@ -0,0 +1,10 @@
+#ejemplo de uso de repositorio
+print("datos personales")
+print(" ")
+vnom = input("Ingrese su nombre: ")
+vedad = input("Ingrese su edad: ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(f"su Nombre es : {vnom} ")
+print(f"Su edad es : {vedad}")
+
+print ("Programa finalizado.....")
